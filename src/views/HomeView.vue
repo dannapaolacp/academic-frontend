eb5f4a929e6bfceafce31eff7876a8d40e8835f2
Danna Paola <dannacapera16@gmail.com>
2023-11-25 20:33:11 -0500

    Load student data on home page using get method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ID</th>
+              <th>Foto</th>
+              <th>NOMBRE</th>
+              <th>APELLIDO</th>
+              <th>REGISTRO</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider" id="content">
+            <!-- content if loading -->
+            <tr v-if="this.loading">
+              <td colspan="6">
+                <h4 class="d-flex justify-content-center">Cargando...</h4>
+              </td>
+            </tr>
+            <!-- content if isn't loading -->
+            <tr v-else v-for="(student, i) in this.students" :key="student.id">
+              <td v-text="i + 1"></td>
+              <td v-text="student.id"></td>
+              <td v-text="student.foto"></td>
+              <td v-text="student.nombre"></td>
+              <td v-text="student.apellido"></td>
+              <!-- friendly date reading -->
+              <td
+                v-text="
+                  new Date(student.created_at).toLocaleDateString('en-US')
+                "
+              ></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  //students data
+  data() {
+    return {
+      students: null,
+      loading: false,
+    };
+  },
+  //if already charged, call the getStudents method
+  mounted() {
+    this.getStudents();
+  },
+  //http methods
+  methods: {
+    getStudents() {
+      this.loading = true;
+      axios
+        .get("http://academicobackend.test/api/v1/estudiantes")
+        .then((res) => {
+          this.students = res.data;
+          this.loading = false;
+        });
+    },
+  },
+};
 </script>
